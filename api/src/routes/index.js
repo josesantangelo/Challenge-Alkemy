9dae11fb7c88bbf017d9bcef0110b30eae83994b
Jose Luis Santangelo <jose.l.santangelo@gmail.com>
2022-03-22 14:50:19 -0300

    express running

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+
+router.get('/', (req, res) => {
+    res.send('works2!')
+})
+
+
 
 
 module.exports = router;
